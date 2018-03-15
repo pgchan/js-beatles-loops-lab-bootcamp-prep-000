bf0a8af6f98e35bdb268b2ad15251d549dce14cd
P G <pgauchan@gmail.com>
2018-03-15 21:13:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function iLoveTheBeatles(number){
   var emptyArray = [];
   do {
     emptyArray.unshift(`I love the Beatles!`);
-  
+    number++;
   } while (number < 15);
   
   return emptyArray;
